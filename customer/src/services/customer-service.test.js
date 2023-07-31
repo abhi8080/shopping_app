ae09d4ef9bd4150b3d2487ba74d2fc29a7dd6d4e
abhi8080 <99359469+abhi8080@users.noreply.github.com>
2023-07-31 13:49:29 +0200

    chore: add extra information to explain clearly

--- customer/src/services/customer-service.test.js
+++ customer/src/services/customer-service.test.js
@@ -1,10 +1,10 @@
 // which service it is
-describe("CustomerService", () => {
-  // Which function we are testing
-  describe("SignIn", () => {
+describe('CustomerService', () => {
+  // This will explain which function we are testing
+  describe('SignIn', () => {
     // Which Scenario we are testing
-    test("validate user inputs", () => {});
+    test('validate user inputs', () => {});
 
-    test("Validate response", async () => {});
+    test('Validate response', async () => {});
   });
 });
